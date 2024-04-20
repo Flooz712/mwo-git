5d6332f27fad130acacd01ba78737356072fff29
Jan Kowalczyk <dsw52712@student.dsw.edu.pl>
2024-04-20 16:07:31 +0200

    Further implementation of the program

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,6 +1,7 @@
 public class SomeProgram {
 	public static void main(String[] args) {
 		System.out.printIn("We are learning to use Git.");
+		System.out.printIn("We will be Git experts, soon.");
 	}
 }
 
